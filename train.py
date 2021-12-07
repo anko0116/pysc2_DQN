9434192b36438ce271b01b907681365807dc5753
hahn <hyukahn@umich.edu>
2021-12-06 23:11:12 -0500

    Able to save model now and load model

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from absl import flags
 
 import tensorflow as tf
 import numpy as np
+import os
 
 from util import compute_avg_return
 from dqn import DeepQNetwork
@@ -24,9 +25,14 @@ batch_size = 32
 
 train_interval = 1
 
-num_train = 500
+num_train = 10000
 
-env = MineralEnv()
+realtime = False
+visualize = False
+use_saved = False
+# End Hyperparameters
+
+env = MineralEnv(realtime, visualize)
 
 agent = DeepQNetwork(
     nS, nA,
@@ -37,11 +43,18 @@ agent = DeepQNetwork(
     batch_size
 )
 
+if use_saved and os.path.exists('training_1.index'):
+    print("Loading saved model")
+    agent.model.load_weights(agent.checkpoint_dir)
+else:
+    print("Traing new model")
+
 for eps in range(num_train):
     print("-----------{} Episode------------".format(eps))
     obs = env.reset()
     done = False
     steps = 0
+    reward = 0
     while not done:
         action = agent.action(obs)
         
@@ -55,7 +68,10 @@ for eps in range(num_train):
 
         obs = new_obs
         steps += 1
-        
-    print(agent.epsilon, steps, agent.loss[-1])
+        reward += rew
+
+    print(agent.epsilon, "|", steps, "|", reward)
+    if len(agent.loss) > 0:
+        print(agent.loss[-1])
 
 env.close()
